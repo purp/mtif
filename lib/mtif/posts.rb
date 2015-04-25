8c6d8d2dcfe0beb1e0fe3063fcece39df8a18d25
Jim Meyer <jim@geekdaily.org>
2015-04-24 19:56:21 -0700

    Add posts with spec

--- lib/mtif/posts.rb
+++ lib/mtif/posts.rb
@@ -1,4 +1,167 @@
+require "time"
+
 class MTIF
   class Post
+    attr_accessor :source, :data
+  
+    SINGLE_VALUE_KEYS = %w(author title basename date body extended_body excerpt
+    keywords allow_comments allow_pings convert_breaks no_entry).map(&:to_sym)
+  
+    MULTILINE_KEYS = %w(body extended_body excerpt keywords comment ping).map(&:to_sym)
+  
+    MULTIVALUE_KEYS = %w(category tag comment ping).map(&:to_sym)
+    
+    VALID_KEYS = (SINGLE_VALUE_KEYS + MULTILINE_KEYS + MULTIVALUE_KEYS).sort.uniq
+
+    DATE_FORMAT = "%m/%d/%Y %I:%M:%S %p"
+
+    def valid_keys
+      VALID_KEYS
+    end
+
+    def valid_key?(key)
+      valid_keys.include?(key.to_sym)
+    end
+
+    def single_line_single_value_keys
+      SINGLE_VALUE_KEYS - MULTILINE_KEYS
+    end
+
+    def single_line_multivalue_keys
+      MULTIVALUE_KEYS - MULTILINE_KEYS
+    end
+
+    def multiline_single_value_keys
+      MULTILINE_KEYS & SINGLE_VALUE_KEYS
+    end
+
+    def multiline_multivalue_keys
+      MULTILINE_KEYS & MULTIVALUE_KEYS
+    end
+
+    def initialize(content)
+      @source = content
+      @data = {}
+    
+      MULTIVALUE_KEYS.each do |key|
+        @data[key] = []
+      end
+    
+      parse_source
+    end
+  
+    def to_mtif
+      result = []
+      single_line_single_value_keys.each do |key|
+        value = self.send(key)
+        next if value.nil? || (value.respond_to?(:empty) && value.empty?)
+
+        result << "#{mtif_key(key)}: #{mtif_value(value)}"
+      end
+
+      single_line_multivalue_keys.each do |key|
+        values = self.send(key)
+        next if values.nil? || (values.respond_to?(:empty) && values.empty?)
+
+        values.each do |value|
+          result << "#{mtif_key(key)}: #{mtif_value(value)}"
+        end
+      end
+
+      multiline_single_value_keys.each do |key|
+        value = self.send(key)
+        next if value.nil? || (value.respond_to?(:empty) && value.empty?)
+
+        result << '-----'
+        result << "#{mtif_key(key)}:\n#{mtif_value(value)}"
+      end
+    
+      multiline_multivalue_keys.each do |key|
+        values = self.send(key)
+        next if values.nil? || (values.respond_to?(:empty) && values.empty?)
+
+        values.each do |value|
+          result << '-----'
+          result << "#{mtif_key(key)}:\n#{mtif_value(value)}"
+        end
+      end
+
+
+      result << '-----' unless result.last == '-----' #close the final field
+      result << '--------' # close the post
+      result.join("\n") + "\n"
+    end
+
+    private
+    def method_missing(method, *args, &block)
+      key = method.to_s.chomp('=').to_sym
+
+      if valid_key?(key)
+        if key = method
+          data[key]
+        else
+          data[key] = args.first
+        end
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method, include_all)
+      key = method.to_s.chomp('=').to_sym
+
+      valid_key?(key) || super
+    end
+
+    def mtif_key_to_key(raw_key)
+      raw_key.strip.downcase.tr(' ','_').to_sym unless raw_key.nil?
+    end
+  
+    def mtif_key(key)
+      key.to_s.tr('_', ' ').upcase
+    end
+
+    def mtif_value(value)
+      value.kind_of?(Time) ? value.strftime(DATE_FORMAT) : value
+    end
+
+    def convert_to_native_type(raw_value)
+      case raw_value
+      when /^\d+$/
+        raw_value.to_i
+      when /^\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}:\d{2} [AP]M/
+        Time.strptime(raw_value, DATE_FORMAT)
+      else
+        raw_value
+      end
+    end
+  
+    def store_data(raw_key, raw_value)
+      key = mtif_key_to_key(raw_key)
+      value = convert_to_native_type(raw_value)
+
+      if MULTIVALUE_KEYS.include?(key)
+        self.data[key] << value unless value.empty?
+      else
+        self.data[key] = value
+      end
+    end
+
+    def parse_source
+      source.slice_before(/-----/).each do |lines|
+        if lines.first =~ /^-----/ && lines.size > 1
+          # Multiline data
+          store_data(lines.shift(2).last.chomp(":\n"), lines.join.strip)
+        elsif lines.first =~ /^[A-Z ]+: /
+          # Single-line data
+          lines.each do |line|
+            unless line.strip.empty?
+              key, value = line.strip.split(": ", 2)
+              store_data(key, value)
+            end
+          end
+        end
+      end
+    end
   end
 end
---
+++ spec/mtif/post_spec.rb
@@ -0,0 +1,149 @@
+RSpec.describe MTIF::Post do
+  it 'should have accessors for source and data' do
+    expect(MTIF::Post.new([])).to respond_to(:source, :data)
+  end
+  
+  context 'valid keys' do
+    it 'should have a list of valid single-value keys' do
+      expect(MTIF::Post).to have_constant('SINGLE_VALUE_KEYS')
+      expect(MTIF::Post::SINGLE_VALUE_KEYS).not_to be_empty
+    end
+
+    it 'should have a list of valid keys which can have multiple values' do
+      expect(MTIF::Post).to have_constant('MULTIVALUE_KEYS')
+      expect(MTIF::Post::MULTIVALUE_KEYS).not_to be_empty
+    end
+
+    it 'should have a list of valid multiline keys' do
+      expect(MTIF::Post).to have_constant('MULTILINE_KEYS')
+      expect(MTIF::Post::MULTILINE_KEYS).not_to be_empty
+    end
+
+    it 'should not have any multivalue keys in the list of single-value keys or vice versa' do
+      expect((MTIF::Post::MULTIVALUE_KEYS & MTIF::Post::SINGLE_VALUE_KEYS)).to be_empty
+    end
+
+    it 'should have a list of valid keys which is proper superset of these' do
+      expect(MTIF::Post).to have_constant('VALID_KEYS')
+      expect(MTIF::Post::VALID_KEYS).to match_array((MTIF::Post::SINGLE_VALUE_KEYS + MTIF::Post::MULTILINE_KEYS + MTIF::Post::MULTIVALUE_KEYS).uniq)
+    end
+    
+    context 'key methods' do
+      before :each do
+        @post = MTIF::Post.new([])
+      end
+      
+      subject {@post}
+
+      it {should respond_to(:valid_keys)}
+      context '#valid_keys' do
+        subject {@post.valid_keys}
+        
+        it {should == MTIF::Post::VALID_KEYS}
+      end
+
+      it {should respond_to(:valid_key?)}
+      context '#valid_key?' do
+        it 'should be true when valid, false otherwise' do
+          valid_key = @post.valid_keys.first.to_sym
+          invalid_key = valid_key.to_s.reverse.to_sym
+
+          expect(@post.valid_key?(valid_key)).to be_truthy
+          expect(@post.valid_key?(valid_key.to_s)).to be_truthy
+          expect(@post.valid_key?(invalid_key)).to be_falsey
+          expect(@post.valid_key?(invalid_key.to_s)).to be_falsey
+        end
+      end
+
+      context 'returning lists' do
+        it {should respond_to(:single_line_single_value_keys)}
+        context '#single_line_single_value_keys' do
+          subject {@post.single_line_single_value_keys}
+          
+          it {should_not include(*MTIF::Post::MULTILINE_KEYS)}
+          it {should_not include(*MTIF::Post::MULTIVALUE_KEYS)}
+          it {should include(*(MTIF::Post::SINGLE_VALUE_KEYS - MTIF::Post::MULTILINE_KEYS))}
+        end
+
+        it {should respond_to(:single_line_multivalue_keys)}
+        context '#single_line_multivalue_keys' do
+          subject {@post.single_line_multivalue_keys}
+          
+          it {should_not include(*MTIF::Post::MULTILINE_KEYS)}
+          it {should_not include(*MTIF::Post::SINGLE_VALUE_KEYS)}
+          it {should include(*(MTIF::Post::MULTIVALUE_KEYS - MTIF::Post::MULTILINE_KEYS))}
+        end
+
+        it {should respond_to(:multiline_single_value_keys)}
+        context '#multiline_single_value_keys' do
+          subject {@post.multiline_single_value_keys}
+          
+          it {should_not include(*MTIF::Post::MULTIVALUE_KEYS)}
+          it {should include(*(MTIF::Post::MULTILINE_KEYS & MTIF::Post::SINGLE_VALUE_KEYS))}
+        end
+
+        it {should respond_to(:multiline_multivalue_keys)}
+        context '#multiline_multivalue_keys' do
+          subject {@post.multiline_multivalue_keys}
+
+          it {should_not include(*MTIF::Post::SINGLE_VALUE_KEYS)}
+          it {should include(*(MTIF::Post::MULTILINE_KEYS & MTIF::Post::MULTIVALUE_KEYS))}
+        end
+      end
+    end
+  end
+
+  context 'fetching data' do
+    subject {MTIF::Post.new([])}
+    
+    MTIF::Post::VALID_KEYS.each do |key|
+      it {should respond_to(key)}
+      it {should respond_to("#{key}=")}
+    end
+
+    it {should_not respond_to(:this_is_not_a_valid_key)}
+    it {should_not respond_to(:this_is_not_a_valid_key=)}
+
+    it 'should default to arrays for multivalue keys' do
+      post = MTIF::Post.new([])
+
+      MTIF::Post::MULTIVALUE_KEYS.each do |key|
+        expect(post.send(key)) == []
+      end
+    end
+  end
+  
+  context '#to_mtif' do
+    before :each do
+      # TODO: refactor to remove ordering dependency
+      @content = [
+        "AUTHOR: The Meyer Kids\n",
+        "TITLE: Crazy Parents: A Primer\n",
+        "DATE: 06/19/1999 07:00:00 PM\n",
+        "ALLOW COMMENTS: 0\n",
+        "CATEGORY: Fun!\n",
+        "-----\n",
+        "BODY:\n",
+        "Start singing an obnoxious song and never stop.\n",
+        "-----\n",
+        "COMMENT:\n",
+        "AUTHOR: Jim Meyer\n",
+        "EMAIL: \n",
+        "IP: 67.180.21.185\n",
+        "URL: http://profile.typepad.com/purp\n",
+        "DATE: 08/26/2010 10:32:04 AM\n",
+        "Yeah, that works.\n",
+        "-----\n",
+        "--------\n"
+      ]
+
+      @post = MTIF::Post.new(@content)
+    end
+    
+    it 'should return concise MTIF containing only keys which are set' do
+      expect(@post).to respond_to(:to_mtif)
+      expect(@post.to_mtif).to eq(@content.join)
+    end
+  end
+  
+end
